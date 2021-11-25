545b3f77597dfb6d937068235faf79d5dfa4562b
Fred <higgins.fred.s@gmail.com>
2021-11-24 19:55:19 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+const hq = 42;
+
+
+//Calculates number of blocks
+function distanceFromHqInBlocks(streetNumber) {
+    if (streetNumber > hq) {
+        return streetNumber - hq;
+    } else if (streetNumber < hq) {
+        return hq - streetNumber;
+    }
+        
+}
+
+
+//Calculates number of feet based on number of blocks
+function distanceFromHqInFeet(streetNumber) {
+    let lengthInFeet = distanceFromHqInBlocks(streetNumber) * 264;
+    return lengthInFeet;
+}
+
+
+//Calculates number of feet to travel
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    } else if (start < destination) {
+        return (destination - start) * 264;
+    }
+}
+
+
+//Calculates fare
+function calculatesFarePrice(start, destination) {
+    let lengthInFeet = distanceTravelledInFeet(start, destination);
+    let fare;
+
+    if (lengthInFeet < 400) {
+        return fare = 0;
+    } else if (lengthInFeet > 400 && lengthInFeet < 2000) {
+        return fare = (lengthInFeet - 400) * .02;
+    } else if (lengthInFeet > 2000 && lengthInFeet < 2500) {
+        return fare = 25;
+    } else if (lengthInFeet > 2500) {
+        return 'cannot travel that far';
+    }
+    
+    return fare;
+}
+
